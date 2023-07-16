1ed260a9fc6812ea1a04205018f4bb2a6b1e3f57
0xSavant <100378695+0xSavant@users.noreply.github.com>
2023-07-16 14:08:25 -0400

    Update: add some comments and fix stupid mistakes

--- tests/stockpile-v2.ts
+++ tests/stockpile-v2.ts
@@ -6,29 +6,36 @@ import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
 describe("stockpile-v2", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const connection = new anchor.web3.Connection(anchor.web3.clusterApiUrl("devnet"));
+
   const program = anchor.workspace.StockpileV2 as Program<StockpileV2>;
 
   it("createProject", async () => {
+    // Generate keypairs for payer, and admins
     const payer = anchor.web3.Keypair.generate();
     let adminKp1 = anchor.web3.Keypair.generate();
     let adminKp2 = anchor.web3.Keypair.generate();
-    let beneficiary = anchor.web3.Keypair.generate().publicKey;
-    let project_id = new Uint8Array(Math.random())
 
+    // Fund payer account
+    connection.requestAirdrop(payer.publicKey, 2);
+
+    // Generate a beneficiary keypair and random projectId
+    let beneficiary = anchor.web3.Keypair.generate().publicKey;
+    let projectId = new Uint8Array(Math.random())
+
+    // Find PDA address
     const [fundraiserPDA, bump] = await anchor.web3.PublicKey.findProgramAddressSync(
-        [utf8.encode("project"), project_id],
+        [utf8.encode("project"), projectId],
         program.programId
     );
 
+    // Define dummy values
     let name = "Sample Project";
-    let admins = [
-      adminKp1.publicKey,
-      adminKp2.publicKey,
-    ];
-
+    let admins = [adminKp1.publicKey, adminKp2.publicKey];
     let goal = 100;
   
-    const tx = await program.methods.createProject(new anchor.BN(project_id), name, admins, beneficiary, goal)
+    // Let it fly
+    const tx = await program.methods.createProject(new anchor.BN(projectId), name, admins, beneficiary, goal)
     .accounts({
       payer: payer.publicKey,
       project: fundraiserPDA,
@@ -36,23 +43,31 @@ describe("stockpile-v2", () => {
     })
     .rpc();
 
+    // If it passes, we get a friendly message
     console.log("🚀 Project Created! Transaction Hash:", tx);
   });
 
   it("createPool", async () => {
+    // Generate payer keypair, and random poolId
     const payer = anchor.web3.Keypair.generate();
-    let pool_id = new Uint8Array(Math.random())
+    let poolId = new Uint8Array(Math.random())
 
+    // Fund payer account
+    connection.requestAirdrop(payer.publicKey, 2);
+
+    // Find PDA address
     const [poolPDA, bump] = await anchor.web3.PublicKey.findProgramAddressSync(
-        [utf8.encode("pool"), pool_id],
+        [utf8.encode("pool"), poolId],
         program.programId
     );
 
+    // Define dummy values
     let name = "Sample Project";
     let start = new anchor.BN(Math.floor(Date.now() / 1000));
     let end = new anchor.BN(Math.floor(Date.now() / 1000) + 30000);
   
-    const tx = await program.methods.createPool(new anchor.BN(pool_id), name, start, end)
+    // Alea iacta est
+    const tx = await program.methods.createPool(new anchor.BN(poolId), name, start, end)
     .accounts({
       payer: payer.publicKey,
       systemProgram: anchor.web3.SystemProgram.programId,
@@ -60,6 +75,7 @@ describe("stockpile-v2", () => {
     })
     .rpc();
 
+    // If it passes, we get a friendly message
     console.log("👾 Funding Round Initialized! Transaction Hash:", tx);
   });
 });
